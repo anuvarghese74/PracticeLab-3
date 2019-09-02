a79d84e24eabba64514c64afc54e8d73675a5f47
Anu Varghese <anuvarghese74@gmail.com>
2019-09-02 12:05:16 +0000

    New Push

---
+++ C_program.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int manin(){
+
+printf("Thank you for writing the firs C program page, visit gain");
+return 0;
+
+
+}
---
+++ HelloJava.java
@@ -0,0 +1,9 @@
+class HelloJava{
+
+public static void main(String args[]){ 
+
+System.out.Println("I;m your java program, thank you!!");
+
+}
+
+}
